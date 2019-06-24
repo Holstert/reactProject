8e07b962dc07c4214a3c6b696895a2035feea2e3
Juan Garcia <juan.garcia@sonorasoft.com>
2019-06-24 08:11:11 -0700

    first message

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { hot } from 'react-hot-loader'
+
+function view() {
+    return (
+        <div>
+            Hello wor
+        </div>
+    )
+}
+
+export default hot(module)(view)
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM, {render} from 'react-dom'
+import App from './app'
+
+const inputRoot = document.getElementById('root');
+
+render(<App />, inputRoot)
